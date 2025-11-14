d65d2b76fbc20179bc00f66a4483ceac3c2c5b67
rpomullan <rpomullan@gmail.com>
2025-11-14 13:30:19 -0600

    some brainstorming

--- cpp/cs441-a5/src/PhysicsEngine.h
+++ cpp/cs441-a5/src/PhysicsEngine.h
@@ -16,18 +16,27 @@ class PhysicsEngine {
 
         
     private:
-        std::vector<std::shared_ptr<Object>> objects;
+
+        // Simulation parameters
         float h;
         float tol;
         int maxiter;
 
+        std::vector<std::shared_ptr<Object>> physicsObjects; 
+        std::vector<int> objectIndicies; // starting index of each physicsObject's point positions in 'positions'
+
+        // Combined properties of all objects
+        Eigen::Matrix3Xf positions;
+        Eigen::Matrix3Xf velocities;
+        std::vector<std::vector<unsigned int>> edgeList;
+
         Eigen::Matrix3Xf getSearchDirection(Eigen::Matrix3Xf& xtilde, float h);
+        void copyToObjects(); // copy the engine positions to each object's OpenGL buffer
         
-        // Helper
+        // Total energy
         float IPValue(Eigen::Matrix3Xf& xtilde, float h);
         Eigen::Matrix3Xf IPGradient(Eigen::Matrix3Xf& xtilde, float h);
         Eigen::MatrixXf IPHessian(Eigen::Matrix3Xf& xtilde, float h);
-        void makePSD(Eigen::MatrixXf& hess);
 
         // Inertia
         float InertiaValue(Eigen::Matrix3Xf& xtilde, float h);
@@ -54,6 +63,23 @@ class PhysicsEngine {
  * to the positions buffers each frame. HOWEVER, this will also make it easier to only draw the visible triangles.
  * 
  * lots to think about...
+ * 
+ * But I also need to consider how later on I will get it to only draw outer triangles (once I improve the cube construction). I suppose
+ * 'object' should be somewhat ignorant to it's 'shape', only dealing with global transformations, holding physics properties, and sending
+ * info to the shaders. So the 'shape' should be what contains the information on which triangles and draw and which are not, which makes
+ * this refactoring a little easier since the construction already happens in 'shape.cpp'
+ * 
+ * The issue is that the current plan for 'PhysicsEngine' is to just copy over the relvant subarrays to their respective objects. Should
+ * 'shape' care about having its internal positions be updated? Or should PhysicsEngine be the one to store it's updated values each step.
+ * It doesn't really need to, since after construction shape is only concered with drawing stuff... so maybe when the PhysicsEngine is 
+ * is initialized it goes and moves that info to itself so that the construction can still be kept in shape. But since after that point we
+ * don't need 'shape' to have it, we just only send back positions that correlate with drawn triangles... not sure how to organize that tho.
+ * if the indices are scattered in position, we can't do bulk copy, so perhaps in 'shape.cpp' functions that construct objects we make sure
+ * to keep a separate array of "trianglePositions" and ensure that the triangles indices of the positions that PhysicsEngine reads from it
+ * are AT THE START OF THE POSITION ARRAY, then we can just read the first chunk of each objects subarray in positions and copy that over. 
+ * 
+ * Maybe it would be easier if all the shapes were just a single buffer that seems like even more refactoring and it doesn't
+ * allow me to apply different shaders to different objects nor apply separate transformations so maybe not.
  * 
  * It might be nice for readability later on to put the energy functions in there own cpp folder but that will have to wait for a 
  * later refactoring (woohoo! technical debt!)
